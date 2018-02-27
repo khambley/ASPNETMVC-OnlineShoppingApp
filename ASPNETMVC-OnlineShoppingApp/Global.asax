eab6d727660ce5327413cf8fc90bb1448c727fc5
Katherine Hambley <superdesigngirl@mac.com>
2018-02-27 11:39:36 -0600

    Add project files.

---
+++ ASPNETMVC-OnlineShoppingApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNETMVC_OnlineShoppingApp.MvcApplication" Language="C#" %>
---
+++ ASPNETMVC-OnlineShoppingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNETMVC_OnlineShoppingApp.Startup))]
+namespace ASPNETMVC_OnlineShoppingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
